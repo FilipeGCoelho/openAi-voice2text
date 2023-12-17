2cb688b6e6985dd4da5dbb13eaa1fc0dd3438e78
Filipe Coelho <filipe.coelho@paradox.ai>
2023-12-17 14:30:42 +0200

    Popup Layout was redesigned. API Key on the UI

--- background.js
+++ background.js
@@ -10,131 +10,141 @@ let popupPort = null;
 let contentPort = null;
 
 chrome.runtime.onConnect.addListener((port) => {
-	if (port.name === POPUP_CONNECTION_NAME) {
-		popupPort = port;
-		console.log(`${PAGE_NAME}: Connected to popup.`);
+  if (port.name === POPUP_CONNECTION_NAME) {
+    popupPort = port;
+    console.log(`${PAGE_NAME}: Connected to popup.`);
 
-		popupPort.postMessage({
-			command: `${POPUP_CONNECTION_NAME}-accepted`,
-			content: "Connection established.",
-		});
+    popupPort.postMessage({
+      command: `${POPUP_CONNECTION_NAME}-accepted`,
+      content: "Connection established.",
+    });
 
-		popupPort.onMessage.addListener((message) => {
-			handlePopupMessage(message);
-		});
+    popupPort.onMessage.addListener((message) => {
+      handlePopupMessage(message);
+    });
 
-		popupPort.onDisconnect.addListener(() => {
-			console.log(`${PAGE_NAME}: Disconnected from popup.`);
-			popupPort = null;
-		});
-	} else if (port.name === CONTENT_CONNECTION_NAME) {
-		contentPort = port;
-		console.log(`${PAGE_NAME}: Connected to content script.`);
+    popupPort.onDisconnect.addListener(() => {
+      console.log(`${PAGE_NAME}: Disconnected from popup.`);
+      popupPort = null;
+    });
+  } else if (port.name === CONTENT_CONNECTION_NAME) {
+    contentPort = port;
+    console.log(`${PAGE_NAME}: Connected to content script.`);
 
-		contentPort.onMessage.addListener((message) => {
-			handleContentMessage(message);
-		});
+    contentPort.onMessage.addListener((message) => {
+      handleContentMessage(message);
+    });
 
-		contentPort.onDisconnect.addListener(() => {
-			console.log(`${PAGE_NAME}: Disconnected from content script.`);
-			contentPort = null;
-		});
-	}
+    contentPort.onDisconnect.addListener(() => {
+      console.log(`${PAGE_NAME}: Disconnected from content script.`);
+      contentPort = null;
+    });
+  }
 });
 
 function handlePopupMessage(message) {
-	if (
-		message.command === COMMAND_START_RECORDING ||
-		message.command === COMMAND_STOP_RECORDING
-	) {
-		sendCommandToContentScript(message.command);
-	}
+  if (
+    message.command === COMMAND_START_RECORDING ||
+    message.command === COMMAND_STOP_RECORDING
+  ) {
+    sendCommandToContentScript(message.command);
+  }
 }
 
 function handleContentMessage(message) {
-	if (message.command === COMMAND_AUDIO_DATA) {
-		handleAudioData(message.data);
-	}
+  if (message.command === COMMAND_AUDIO_DATA) {
+    handleAudioData(message.data);
+  }
 }
 
 function sendCommandToContentScript(command) {
-	chrome.windows.getCurrent((w) => {
-		chrome.tabs.query({ active: true, windowId: w.id }, function (tabs) {
-			if (tabs.length === 0) {
-				console.error(`${PAGE_NAME}: No active tab found.`);
-				return;
-			}
-			if (contentPort) {
-				contentPort.postMessage({ command });
-				console.log(
-					`${PAGE_NAME}: command '${command} send to contentScript`
-				);
-			} else console.error("ContentScript.js connection fell.");
-		});
-	});
+  chrome.windows.getCurrent((w) => {
+    chrome.tabs.query({ active: true, windowId: w.id }, function (tabs) {
+      if (tabs.length === 0) {
+        console.error(`${PAGE_NAME}: No active tab found.`);
+        return;
+      }
+      if (contentPort) {
+        contentPort.postMessage({ command });
+        console.log(`${PAGE_NAME}: command '${command} send to contentScript`);
+      } else console.error("ContentScript.js connection fell.");
+    });
+  });
 }
 
 function sendTranscriptionToPopup(transcription) {
-	if (popupPort) {
-		popupPort.postMessage({
-			command: COMMAND_TRANSCRIPTION_RESULT,
-			transcription,
-		});
-		console.log(`${PAGE_NAME}: Transcription result sent to popup.`);
-	} else {
-		console.error(`${PAGE_NAME}: No active connection to popup.`);
-	}
+  if (popupPort) {
+    popupPort.postMessage({
+      command: COMMAND_TRANSCRIPTION_RESULT,
+      transcription,
+    });
+    console.log(`${PAGE_NAME}: Transcription result sent to popup.`);
+  } else {
+    console.error(`${PAGE_NAME}: No active connection to popup.`);
+  }
 }
 
 function handleAudioData(dataURL) {
-	const blob = dataURLToBlob(dataURL);
-	sendToTranscriptionAPI(blob);
+  const blob = dataURLToBlob(dataURL);
+  sendToTranscriptionAPI(blob);
 }
 
 function dataURLToBlob(dataURL) {
-	const byteString = atob(dataURL.split(",")[1]);
-	const mimeString = dataURL.split(",")[0].split(":")[1].split(";")[0];
-	const ab = new ArrayBuffer(byteString.length);
-	const ia = new Uint8Array(ab);
+  const byteString = atob(dataURL.split(",")[1]);
+  const mimeString = dataURL.split(",")[0].split(":")[1].split(";")[0];
+  const ab = new ArrayBuffer(byteString.length);
+  const ia = new Uint8Array(ab);
 
-	for (let i = 0; i < byteString.length; i++) {
-		ia[i] = byteString.charCodeAt(i);
-	}
+  for (let i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
 
-	return new Blob([ab], { type: mimeString });
+  return new Blob([ab], { type: mimeString });
+}
+
+function useApiKey(callback) {
+  chrome.storage.local.get("apiKey", function (result) {
+    if (result.apiKey) {
+      callback(result.apiKey);
+    } else {
+      console.error("No API Key found");
+      // Handle the absence of the API key appropriately
+    }
+  });
 }
 
 function sendToTranscriptionAPI(blob) {
-	const formData = new FormData();
-	formData.append("file", blob, "audio.webm");
-	formData.append("model", "whisper-1");
-	formData.append("language", "en");
-	formData.append("response_format", "json");
-	formData.append("temperature", 0.2); // Adjust as needed
+  useApiKey((apiKey) => {
+    const formData = new FormData();
+    formData.append("file", blob, "audio.webm");
+    formData.append("model", "whisper-1");
+    formData.append("language", "en");
+    formData.append("response_format", "json");
+    formData.append("temperature", 0.2); // Adjust as needed
 
-	// Replace with your actual API endpoint and API key
-	const apiEndpoint = "https://api.openai.com/v1/audio/transcriptions";
-	const apiKey = "REPLACE-WITH-YOUR-OPENAI-KEY";
+    // Replace with your actual API endpoint and API key
+    const apiEndpoint = "https://api.openai.com/v1/audio/transcriptions";
 
-	fetch(apiEndpoint, {
-		method: "POST",
-		headers: {
-			Authorization: `Bearer ${apiKey}`,
-		},
-		body: formData,
-	})
-		.then((response) => {
-			if (!response.ok) {
-				throw new Error(`API response error: ${response.statusText}`);
-			}
-			return response.json();
-		})
-		.then((data) => {
-			console.log("Transcription result:", data.text);
-			// Process the transcription result
-			sendTranscriptionToPopup(data.text);
-		})
-		.catch((error) => {
-			console.error("Error sending audio to transcription API:", error);
-		});
+    fetch(apiEndpoint, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: formData,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`API response error: ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Transcription result:", data.text);
+        // Process the transcription result
+        sendTranscriptionToPopup(data.text);
+      })
+      .catch((error) => {
+        console.error("Error sending audio to transcription API:", error);
+      });
+  });
 }
--- popup.js
+++ popup.js
@@ -7,65 +7,80 @@ let isRecording = false;
 let port = null;
 
 document.addEventListener("DOMContentLoaded", () => {
-	port = chrome.runtime.connect({ name: CONNECTION_WITH_BACKGROUND_NAME });
+  port = chrome.runtime.connect({ name: CONNECTION_WITH_BACKGROUND_NAME });
 
-	setupRecordStopButton();
-	setupClipboardButton();
+  setupRecordStopButton();
+  setupClipboardButton();
+  setupPasswordSaveButton();
 
-	// Listen for messages from the background script
-	port.onMessage.addListener((message) => {
-		if (message.command === `${CONNECTION_WITH_BACKGROUND_NAME}-accepted`) {
-			console.log(
-				`${PAGE_NAME}: connection ${CONNECTION_WITH_BACKGROUND_NAME} accepted: ${message.content}`
-			);
-		} else if (message.command === `${COMMAND_START_RECORDING}-accepted`) {
-			console.log(
-				`${PAGE_NAME}: request ${COMMAND_START_RECORDING} accepted: ${message.content}`
-			);
-		} else if (message.command === `${COMMAND_STOP_RECORDING}-accepted`) {
-			console.log(
-				`${PAGE_NAME}: request ${COMMAND_STOP_RECORDING} accepted: ${message.content}`
-			);
-		} else if (message.command === COMMAND_TRANSCRIPTION_RESULT) {
-			document.getElementById(COMMAND_TRANSCRIPTION_RESULT).textContent =
-				message.transcription || "No transcription available.";
-		}
-	});
+  // Listen for messages from the background script
+  port.onMessage.addListener((message) => {
+    if (message.command === `${CONNECTION_WITH_BACKGROUND_NAME}-accepted`) {
+      console.log(
+        `${PAGE_NAME}: connection ${CONNECTION_WITH_BACKGROUND_NAME} accepted: ${message.content}`
+      );
+    } else if (message.command === `${COMMAND_START_RECORDING}-accepted`) {
+      console.log(
+        `${PAGE_NAME}: request ${COMMAND_START_RECORDING} accepted: ${message.content}`
+      );
+    } else if (message.command === `${COMMAND_STOP_RECORDING}-accepted`) {
+      console.log(
+        `${PAGE_NAME}: request ${COMMAND_STOP_RECORDING} accepted: ${message.content}`
+      );
+    } else if (message.command === COMMAND_TRANSCRIPTION_RESULT) {
+      document.getElementById(COMMAND_TRANSCRIPTION_RESULT).textContent =
+        message.transcription || "No transcription available.";
+    }
+  });
 });
 
 function setupRecordStopButton() {
-	const button = document.getElementById("record-toggle");
-	button.addEventListener("click", () => {
-		isRecording = !isRecording; // Toggle the recording state
-		updateRecordingState(button, isRecording);
-	});
+  const button = document.getElementById("record-toggle");
+  button.addEventListener("click", () => {
+    isRecording = !isRecording; // Toggle the recording state
+    updateRecordingState(button, isRecording);
+  });
 }
 
 function updateRecordingState(button, isRecording) {
-	if (isRecording) {
-		button.textContent = "Stop Recording";
-		port.postMessage({ command: COMMAND_START_RECORDING });
-	} else {
-		button.textContent = "Start Recording";
-		port.postMessage({ command: COMMAND_STOP_RECORDING });
-	}
+  if (isRecording) {
+    button.textContent = "Stop Recording";
+    port.postMessage({ command: COMMAND_START_RECORDING });
+  } else {
+    button.textContent = "Start Recording";
+    port.postMessage({ command: COMMAND_STOP_RECORDING });
+  }
 }
 
 function setupClipboardButton() {
-	document
-		.getElementById("copy-to-clipboard")
-		.addEventListener("click", () => {
-			const textToCopy = document.getElementById(
-				COMMAND_TRANSCRIPTION_RESULT
-			).innerText;
-			navigator.clipboard
-				.writeText(textToCopy)
-				.then(() => {
-					// Optionally, notify the user that the text was copied.
-					console.log("Text copied to clipboard");
-				})
-				.catch((err) => {
-					console.error("Error in copying text: ", err);
-				});
-		});
+  document.getElementById("copy-to-clipboard").addEventListener("click", () => {
+    const textToCopy = document.getElementById(
+      COMMAND_TRANSCRIPTION_RESULT
+    ).innerText;
+    navigator.clipboard
+      .writeText(textToCopy)
+      .then(() => {
+        // Optionally, notify the user that the text was copied.
+        console.log("Text copied to clipboard");
+      })
+      .catch((err) => {
+        console.error("Error in copying text: ", err);
+      });
+  });
+}
+
+function setupPasswordSaveButton() {
+  document.getElementById("saveButton").addEventListener("click", function () {
+    const apiKeyInput = document.getElementById("apiKeyInput");
+    const apiKey = apiKeyInput.value;
+
+    chrome.storage.local.set({ apiKey: apiKey }, function () {
+      if (chrome.runtime.lastError) {
+        console.error("Error saving API Key:", chrome.runtime.lastError);
+      } else {
+        apiKeyInput.value = "";
+        console.log("API Key saved successfully");
+      }
+    });
+  });
 }
